dca871747758e9f31eabb41f6cd1eef38ac2e3a1
abu00123 <a.garang@alustudent.com>
2024-07-15 11:43:35 -0700

    work

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module with a func that returns attrs and mthds of an object:."""
+
+
+def lookup(obj):
+    """Return a list of avail attrs nd mtds."""
+    return (dir(obj))#!/usr/bin/python3
+"""Module with a func that returns attrs and mthds of an object:."""
+
+
+def lookup(obj):
+        """Return a list of avail attrs nd mtds."""
+            return (dir(obj))#!/usr/bin/python3
+        """Module with a func that returns attrs and mthds of an object:."""
+
+
+        def lookup(obj):
+                """Return a list of avail attrs nd mtds."""
+                    return (dir(obj))#!/usr/bin/python3
+                """Module with a func that returns attrs and mthds of an object:."""
+
+
+                def lookup(obj):
+                        """Return a list of avail attrs nd mtds."""
+                            return (dir(obj))#!/usr/bin/python3
+                        """Module with a func that returns attrs and mthds of an object:."""
+
+
+                        def lookup(obj):
+                                """Return a list of avail attrs nd mtds."""
+                                    return (dir(obj))#!/usr/bin/python3
+                                """Module with a func that returns attrs and mthds of an object:."""
+
+
+                                def lookup(obj):
+                                        """Return a list of avail attrs nd mtds."""
+                                            return (dir(obj))#!/usr/bin/python3
+                                        """Module with a func that returns attrs and mthds of an object:."""
+
+
+                                        def lookup(obj):
+                                                """Return a list of avail attrs nd mtds."""
+                                                    return (dir(obj))
